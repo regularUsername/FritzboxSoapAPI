436fd2c7cfcdff3affc4cf6ec5a1426c99f60c7c
unknown <16479865+regularUsername@users.noreply.github.com>
2020-02-16 15:31:21 +0100

    create soap_action methods at runtime

--- FritzBoxServices.py
+++ FritzBoxServices.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 from bs4 import BeautifulSoup, Tag
+from fritzbox_soap import soap_action
 
 
 class Services:
     def __init__(self):
+        self._baseURL = "https://fritz.box:40888/tr064"
         with Path("./SCPD_Files/tr64desc.xml").open("r") as fp:
             soup = BeautifulSoup(fp.read(), "lxml-xml")
 
@@ -13,18 +15,21 @@ class Services:
                 name = service.serviceType.text.split(':')[-2]
                 friendlyName = name.replace('X_AVM-DE_', '')
                 self._services[friendlyName] = {x.name: x.text.strip()
-                                        for x in service if isinstance(x, Tag)}
+                                                for x in service if isinstance(x, Tag)}
 
     def __getattr__(self, name):
         if name in self._services.keys():
-            return Service(name, self._services[name]['SCPDURL'])
+            service = self._services[name]
+            return Service(name,
+                           service['serviceType'],
+                           self._baseURL+service['controlURL'],
+                           service['SCPDURL'])
         raise AttributeError(f"'Services' object has no attribute '{name}'")
 
     def listServices(self):
         return list(self._services.keys())
 
     def serviceInfo(self, name):
-        # TODO error handling
         if name in self._services.keys():
             s = self._services[name]
             ret = f"""Service: {name}
@@ -35,15 +40,16 @@ class Services:
     scpd url:       '{s['SCPDURL']}'
 """
             return ret
-        else:
-            raise ValueError('bad method')
 
+        raise ValueError('bad method')
 
 
 class Service:
-    def __init__(self, serviceType, scpd_url):
+    def __init__(self, name, serviceType, controlURL, scpdUrl):
+        self._serviceName = name
         self._serviceType = serviceType
-        with Path("./SCPD_Files"+scpd_url).open('r') as fp:
+        self._controlURL = controlURL
+        with Path("./SCPD_Files"+scpdUrl).open('r') as fp:
             soup = BeautifulSoup(fp.read(), "lxml-xml")
 
         self._actions = {}
@@ -58,22 +64,34 @@ class Service:
                     returnvalues.append(argname)
                 else:
                     args.append(argname)
-            self._actions[friendlyName] = {'name': name, 'arguments': args, 'returnvalues':returnvalues}
-
-        # TODO parse SCPD file
+            self._actions[friendlyName] = {
+                'name': name, 'arguments': args, 'returnvalues': returnvalues}
 
     def __getattr__(self, name):
         if name in self._actions.keys():
-            return lambda: print(name)
-        raise AttributeError(f"'Service' object has no attribute '{name}'")
+            action = self._actions[name]
+
+            def f(**kwargs):
+                soap = soap_action(self._controlURL,
+                                   self._serviceType,
+                                   action['name'],
+                                   kwargs)
+
+                #TODO parse soap response and return results as dict
+                return soap
+
+            return f
+
+        s = f"'{self._serviceName}-Service' object has no attribute '{name}'"
+        raise AttributeError(s)
 
     def __repr__(self):
-        return f"<class 'Service: {self._serviceType}>"
+        return f"<class 'Service: {self._serviceName}>"
 
     def listMethods(self):
         return list(self._actions.keys())
 
-    # wie in https://docs.python.org/3/library/xmlrpc.client.html#serverproxy-objects
+    # wie in https://docs.python.org/3/library/xmlrpc.client.html#serverproxy-objects ?
     def methodSignature(self, name):
         raise NotImplementedError
 
@@ -86,14 +104,14 @@ class Service:
             s += f":return {{{', '.join(rets)}}}"
 
             return s
-        else:
-            raise ValueError('bad method')
+
+        raise ValueError('bad method')
 
 
 if __name__ == "__main__":
     services = Services()
     print(services.listServices())
-    print(services.serviceInfo('Homeplug'))
-    print(services.Homeplug.listMethods())
-    print(services.Homeplug.methodHelp('GetGenericDeviceEntry'))
-    # print(services.Hosts.SetHostNameByMACAddress)
+    print(services.serviceInfo('Homeauto'))
+    print(services.Homeauto.listMethods())
+    print(services.Homeauto.methodHelp('GetGenericDeviceInfos'))
+    # print(services.Homeauto.GetGenericDeviceInfos(NewIndex=0))
